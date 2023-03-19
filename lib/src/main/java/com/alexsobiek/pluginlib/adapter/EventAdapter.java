13be5a481e90ad081e63d85a887da18f3039366d
Alex Sobiek <alex@alexsobiek.com>
2023-03-18 19:50:39 -0500

    Added event listen methods

--- lib/src/main/java/com/alexsobiek/pluginlib/adapter/EventAdapter.java
+++ lib/src/main/java/com/alexsobiek/pluginlib/adapter/EventAdapter.java
@@ -3,15 +3,13 @@ package com.alexsobiek.pluginlib.adapter;
 import com.alexsobiek.nexus.inject.annotation.Inject;
 import com.alexsobiek.pluginlib.AbstractPlugin;
 import lombok.Getter;
-import org.bukkit.event.Event;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.HandlerList;
-import org.bukkit.event.Listener;
+import org.bukkit.event.*;
 
 import java.io.Closeable;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 
 public abstract class EventAdapter<P extends AbstractPlugin> implements Adapter<P>, Listener {
     private final List<Closeable> closeables = new ArrayList<>();
@@ -37,9 +35,49 @@ public abstract class EventAdapter<P extends AbstractPlugin> implements Adapter<
         }
     }
 
-    protected <T extends Closeable> T register(T closeable) {
+    public static <E extends Event> Closeable listen(AbstractPlugin plugin, Class<E> event, EventPriority priority, boolean ignoreCancelled, Consumer<E> consumer) {
+        Listener listener = new Listener() {
+        };
+
+        plugin.getServer().getPluginManager().registerEvent(event, listener, priority, (l, e) -> {
+            if (e.getClass().equals(event)) consumer.accept(event.cast(e));
+        }, plugin, ignoreCancelled);
+
+        return () -> {
+            try {
+                Method getHandlerListMethod = event.getDeclaredMethod("getHandlerList");
+                getHandlerListMethod.setAccessible(true);
+                HandlerList handlers = (HandlerList) getHandlerListMethod.invoke(null);
+                handlers.unregister(listener);
+            } catch (Throwable t) {
+                plugin.logger().error("Failed to unregister listener for event {}", event.getSimpleName(), t);
+            }
+        };
+    }
+
+    public static <E extends Event> Closeable listen(AbstractPlugin plugin, Class<E> event, EventPriority priority, Consumer<E> consumer) {
+        return listen(plugin, event, priority, false, consumer);
+    }
+
+    public static <E extends Event> Closeable listen(AbstractPlugin plugin, Class<E> event, Consumer<E> consumer) {
+        return listen(plugin, event, EventPriority.NORMAL, false, consumer);
+    }
+
+    protected <T extends Closeable> T closeable(T closeable) {
         closeables.add(closeable);
         return closeable;
+    }
+
+    public <E extends Event> void listen(Class<E> event, EventPriority priority, boolean ignoreCancelled, Consumer<E> consumer) {
+        closeable(listen(getPlugin(), event, priority, ignoreCancelled, consumer));
+    }
+
+    public <E extends Event> void listen(Class<E> event, EventPriority priority, Consumer<E> consumer) {
+        closeable(listen(getPlugin(), event, priority, consumer));
+    }
+
+    public <E extends Event> void listen(Class<E> event, Consumer<E> consumer) {
+        closeable(listen(getPlugin(), event, consumer));
     }
 
     private void unregisterListeners() {
--- test-plugin/src/main/java/com/alexsobiek/pluginlib/test/TestPlugin.java
+++ test-plugin/src/main/java/com/alexsobiek/pluginlib/test/TestPlugin.java
@@ -2,13 +2,26 @@ package com.alexsobiek.pluginlib.test;
 
 import com.alexsobiek.pluginlib.AbstractPlugin;
 import com.alexsobiek.pluginlib.adapter.EventAdapter;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 public class TestPlugin extends AbstractPlugin {
     @Override
     public void enable() {
         TestEventAdapter adapter = EventAdapter.register(TestEventAdapter.class, this);
         logger().info("Registered adapter: {}", adapter);
-        adapter.disable();
+
+        AutoCloseable listener = EventAdapter.listen(this, PlayerJoinEvent.class, event -> {
+            event.getPlayer().sendMessage("Hello from the test plugin!");
+        });
+
+        getServer().getScheduler().runTaskLater(this, () -> {
+            try {
+                listener.close();
+                System.out.println("Closed listener");
+            } catch (Throwable t) {
+                logger().error("Failed to close listener", t);
+            }
+        }, 400L);
     }
 
     @Override
